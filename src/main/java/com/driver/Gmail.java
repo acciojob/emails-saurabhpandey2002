951a6762c9fb70bb647ff703b3de3f7c7c711266
Saurabh Pandey <saurabhpandey200200@gmail.com>
2024-02-24 20:30:08 +0530

    add1

--- src/main/java/com/driver/Gmail.java
+++ src/main/java/com/driver/Gmail.java
@@ -1,15 +1,16 @@
 package com.driver;
 
-import java.util.ArrayList;
-import java.util.Date;
+import java.util.*;
+
 
 public class Gmail extends Email {
 
-    private ArrayList<Mail> inbox;
-    private ArrayList<Mail> trash;
     int inboxCapacity; //maximum number of mails inbox can store
     //Inbox: Stores mails. Each mail has date (Date), sender (String), message (String). It is guaranteed that message is distinct for all mails.
     //Trash: Stores mails. Each mail has date (Date), sender (String), message (String)
+    private ArrayList<Address> inbox;
+    private ArrayList<Address> trash;
+
     public Gmail(String emailId, int inboxCapacity) {
         super(emailId);
         this.inboxCapacity = inboxCapacity;
@@ -22,49 +23,49 @@ public class Gmail extends Email {
         // It is guaranteed that:
         // 1. Each mail in the inbox is distinct.
         // 2. The mails are received in non-decreasing order. This means that the date of a new mail is greater than equal to the dates of mails received already.
-        if (inbox.size() >= inboxCapacity) {
-            trash.add(inbox.remove(0));
+        if(inbox.size()>=getInboxCapacity()){
+            trash.add(inbox.get(0));
+            inbox.remove(0);
         }
-        inbox.add(new Mail(date, sender, message));
+        inbox.add(new Address(message,date,sender));
     }
 
     public void deleteMail(String message){
         // Each message is distinct
         // If the given message is found in any mail in the inbox, move the mail to trash, else do nothing
-        for (Mail mail : inbox) {
-            if (mail.getMessage().equals(message)) {
-                trash.add(mail);
-                inbox.remove(mail);
+        for(int i =0;i<inbox.size();i++){
+            Address m = inbox.get(i);
+            //if(m.message==null) continue;
+            if(m.message.equals(message)){
+                trash.add(m);
+                inbox.remove(i);
                 return;
             }
         }
+
     }
 
     public String findLatestMessage(){
         // If the inbox is empty, return null
         // Else, return the message of the latest mail present in the inbox
-        if (!inbox.isEmpty()) {
-            return inbox.get(inbox.size() - 1).getMessage();
-        }
-        return null;
+        if(inbox.size()==0) return null;
+        else return inbox.get(inbox.size()-1).message;
     }
 
     public String findOldestMessage(){
         // If the inbox is empty, return null
         // Else, return the message of the oldest mail present in the inbox
-        if (!inbox.isEmpty()) {
-            return inbox.get(0).getMessage();
-        }
-        return null;
-
+        if(inbox.size()==0) return null;
+        else return inbox.get(0).message;
     }
 
     public int findMailsBetweenDates(Date start, Date end){
         //find number of mails in the inbox which are received between given dates
         //It is guaranteed that start date <= end date
         int count = 0;
-        for (Mail mail : inbox) {
-            if (mail.getDate().compareTo(start) >= 0 && mail.getDate().compareTo(end) <= 0) {
+        for(int i =0;i<inbox.size();i++){
+            Address m = inbox.get(i);
+            if(m.date.compareTo(start)>=0 && m.date.compareTo(end)<=0){
                 count++;
             }
         }
@@ -88,29 +89,16 @@ public class Gmail extends Email {
 
     public int getInboxCapacity() {
         // Return the maximum number of mails that can be stored in the inbox
-        return inboxCapacity;
+        return this.inboxCapacity;
     }
-    private static class Mail {
-        private Date date;
-        private String sender;
-        private String message;
-
-        public Mail(Date date, String sender, String message) {
+    private class Address{
+        String sender;
+        Date date;
+        String message;
+        Address(String message, Date date,String sender ){
             this.date = date;
             this.sender = sender;
             this.message = message;
-        }
-
-        public Date getDate() {
-            return date;
-        }
-
-        public String getSender() {
-            return sender;
-        }
-
-        public String getMessage() {
-            return message;
         }
     }
 }
